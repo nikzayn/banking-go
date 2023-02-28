f372974cb1b3a9197c03742f558d0874ffb1b519
nikzayn <nikhilvaidyar1997@gmail.com>
2023-02-28 13:46:37 +0530

    FindAll customer function --added

--- domain/customerRepositoryDb.go
+++ domain/customerRepositoryDb.go
@@ -2,13 +2,56 @@ package domain
 
 import (
 	"database/sql"
+	"log"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
+// Define CustomerRepositoryDb struct with client as sql db type
 type CustomerRepositoryDb struct {
+	client *sql.DB
 }
 
-func (d CustomerRepositoryDb) FindAll() ([]Customer, error) {
-	db, err := sql.Open("mysql", "root:nikhil123")
+/*
+* Description - FindAll function is to find all the customers from the customers table
+	* @Output -> (Customer struct, error)
+*/
+func (db CustomerRepositoryDb) FindAll() ([]Customer, error) {
+	findAllSql := `select * from customers`
+
+	//Query rows with findAllSql query
+	rows, err := db.client.Query(findAllSql)
+	if err != nil {
+		log.Println("Error while querying customer table. " + err.Error())
+		return nil, err
+	}
+
+	customers := make([]Customer, 0)
+	for rows.Next() {
+		var c Customer
+		err := rows.Scan(&c.Id, &c.Name, &c.City, &c.DateOfBirth, &c.Status, &c.Zipcode)
+		if err != nil {
+			log.Println("Error while scanning customers. " + err.Error())
+			return nil, err
+		}
+		customers = append(customers, c)
+	}
+	return customers, nil
+}
+
+/*
+* Description - Creating DB Pool connection
+	* @Output -> CustomerRepositoryDb struct
+*/
+func NewCustomerRepositoryDb() CustomerRepositoryDb {
+	client, err := sql.Open("mysql", "root:nikhil123@tcp(127.0.0.1:3306)/banking")
+	if err != nil {
+		panic(err)
+	}
+
+	client.SetConnMaxLifetime(time.Minute * 3)
+	client.SetMaxOpenConns(10)
+	client.SetMaxIdleConns(10)
+	return CustomerRepositoryDb{client}
 }
